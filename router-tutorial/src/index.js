b957bb3085f45563bd0e9658d755119673d93143
Gregory Hawman <g.hawman@gmail.com>
2021-11-30 16:35:50 -0500

    finished the tutorial should be pretty comfortable react-routing again

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import Expenses from './routes/expenses'
 import Invoices from './routes/invoices'
+import Invoice from './routes/invoice'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -13,7 +14,25 @@ ReactDOM.render(
         <Routes>
           <Route path='/' element={<App />}>
             <Route path='expenses' element={<Expenses />} />
-            <Route path='invoices' element={<Invoices />} />
+            <Route path='invoices' element={<Invoices />}>
+              <Route 
+                index
+                element={
+                  <main style={{ padding: '1rem' }}>
+                    <p>Select an Invoice</p>
+                  </main>
+                }
+              />
+              <Route path=':invoiceId' element={<Invoice />}/>
+            </Route>
+            <Route 
+              path='*'
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>There's Nothing Here! Go Back</p>
+                </main>
+              }
+            />
           </Route>
         </Routes>
     </BrowserRouter>
